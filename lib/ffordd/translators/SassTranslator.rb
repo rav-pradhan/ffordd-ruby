a25078289ef372f71750beb0d8302c4e51856e99
Ravi <ravipradhan@protonmail.com>
2021-04-02 18:10:28 +0100

    refactor .each to .map

--- lib/ffordd/translators/SassTranslator.rb
+++ lib/ffordd/translators/SassTranslator.rb
@@ -4,20 +4,15 @@ class SassTranslator
     end
 
     def translate()
-        output = []
-        @input.each do |category, category_value|
+        @input.map { |category, category_value|
             category_tokens = tokenise_category_values(category_value)
             result = {'$' + category => category_tokens}
-            output.push(result)
-        end
-        return output
+        }
     end
 
     def tokenise_category_values(category_value)
-        tokens = []
-        category_value.each do |property, value|
-            tokens.push("\t'$#{property}': #{value}\n")
-        end
-        return tokens
+        category_value.map { |property, value|
+            result = ("\t'$#{property}': #{value}\n")
+        }
     end
 end
