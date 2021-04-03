27cc1b91479d8a16d15da75c8fb437c1f8252b08
Ravi <ravipradhan@protonmail.com>
2021-04-03 14:52:58 +0100

    add error handling for empty input

--- lib/ffordd/translators/SassTranslator.rb
+++ lib/ffordd/translators/SassTranslator.rb
@@ -4,6 +4,18 @@ class SassTranslator
     end
 
     def translate
+        unless @input.empty?
+            tokenise_input
+        else
+            throw_error(NO_INPUT_PROVIDED)
+        end       
+    end
+
+    private
+
+    NO_INPUT_PROVIDED = "No input provided for translation"
+
+    def tokenise_input
         @input.map do |category, category_value|
             category_tokens = tokenise_category_values(category_value)
             {'$' + category => category_tokens}
@@ -11,8 +23,13 @@ class SassTranslator
     end
 
     def tokenise_category_values(category_value)
-        category_value.map { |property, value|
-            ("\t'$#{property}': #{value}\n")
-        }
+        category_value.map do |property, value|
+            ("\t'$#{property}': #{value}")
+        end
     end
+
+    def throw_error(message)
+        StandardError.new message
+    end
+
 end
--- spec/ffordd/translators/sass_translator_spec.rb
+++ spec/ffordd/translators/sass_translator_spec.rb
@@ -4,22 +4,43 @@ require "json"
 describe "SassTranslator" do
 
     describe ".translate" do
-        context "when translate is invoked with a design token category that has a token" do
-            let(:mock_input) { JSON.parse('{"colours": {"dark": "#121111"}}') }
-            let(:output) { SassTranslator.new(mock_input).translate() }
 
-            it "correctly translates it into a hash map with one Sass list" do
-                result = [{"$colours"=>["\t\'$dark\': #121111\n"]}]
+        context "when an empty input is provided" do
+            let(:empty_input) { JSON.parse('{}')}
+            let(:output) { SassTranslator.new(empty_input).translate() }
+
+            it "throws an empty input error" do
+                result = StandardError.new "No input provided for translation"
                 expect(output).to eq(result)
             end
         end
 
-        context "when translate is invoked with two token categories that has one token each" do
+        context "when invoked with a token category that has one token" do
+            let(:mock_input) { JSON.parse('{"colours": {"dark": "#121111"}}') }
+            let(:output) { SassTranslator.new(mock_input).translate() }
+
+            it "correctly translates it into a hash map with one Sass list" do
+                result = [{"$colours"=>["\t\'$dark\': #121111"]}]
+                expect(output).to eq(result)
+            end
+        end
+
+        context "when invoked with a token category that has multiple tokens" do
+            let(:mock_input) { JSON.parse('{"colours": {"dark": "#121111", "light": "#fff"}}')}
+            let(:output) { SassTranslator.new(mock_input).translate() }
+
+            it "correctly translates into a hash map with two elements" do
+                result = [{"$colours"=>["\t\'$dark\': #121111", "\t'$light': #fff"]}]
+                expect(output).to eq(result)
+            end
+        end
+
+        context "when invoked with two token categories that has one token each" do
             let(:mock_input) { JSON.parse('{"colours": {"dark": "#121111"}, "fonts": {"content": "Times New Roman"}}') }
             let(:output) { SassTranslator.new(mock_input).translate() }
 
             it "correctly translates them into a hash map with two Sass lists" do
-                result = [{"$colours"=>["\t\'$dark\': #121111\n"]}, {"$fonts"=>["\t\'$content\': Times New Roman\n"]}]
+                result = [{"$colours"=>["\t\'$dark\': #121111"]}, {"$fonts"=>["\t\'$content\': Times New Roman"]}]
                 expect(output).to eq(result)
             end
         end
